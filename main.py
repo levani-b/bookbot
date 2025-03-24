b23fa686c7127e9cb94215f43401993529d337d8
levani-b <levanbirchadze@gmail.com>
2025-03-24 11:01:31 +0400

    add report functions

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ def get_sys_argvs():
         print('Usage: python3 main.py <path_to_book>')
         sys.exit(1)
     return sys.argv[1]
+
+def print_report(book_path, num_words, char_sorted_list):
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {book_path}...')
+    print('----------- Word Count ----------')
+    print(f'Found {num_words} total words')
+    print('--------- Character Count -------')
+    
+
+    for char in char_sorted_list:
+        if char['char'].isalpha():
+            print(f'{char["char"]}: {char["count"]}')
+
+    print('============= END ===============')
     
 def main():
     book_path = get_sys_argvs()  
@@ -19,18 +33,7 @@ def main():
     count_of_words  = count_words(text)
     char_count = count_chars(text)
     sorted_char_count = sort_dict(char_count)
+    print_report(book_path,count_of_words, sorted_char_count)
 
-    print('============ BOOKBOT ============')
-    print(f'Analyzing book found at {book_path}...')
-    print('----------- Word Count ----------')
-    print(f'Found {count_of_words} total words')
-    print('--------- Character Count -------')
-    
-
-    for char in sorted_char_count:
-        if char['char'].isalpha():
-            print(f'{char["char"]}: {char["count"]}')
-
-    print('============= END ===============')
 
 main()
